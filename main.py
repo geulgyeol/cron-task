1be76f7428a2b1e10e1e1b4b9d45c7f888c96173
devngho <yjh135908@gmail.com>
2025-11-29 01:11:28 +0900

    init

---
+++ main.py
@@ -0,0 +1,54 @@
+import os
+import re
+from huggingface_hub import HfApi
+import requests
+
+HF_TOKEN = os.getenv("HF_TOKEN") # will be injected by kubernetes secret
+if not HF_TOKEN:
+    raise ValueError("HF_TOKEN environment variable is not set.")
+HTML_STORAGE_PATH: str = "html-storage.default.svc.cluster.local"
+SPACE_NAME = "geulgyeol/README"
+
+hf_api = HfApi(token=HF_TOKEN)
+
+def fetch_files_data():
+    url = f"http://{HTML_STORAGE_PATH}/files"
+    response = requests.get(url)
+    response.raise_for_status()
+
+    return response.json()
+
+def get_updated_page_content(old_page: str, new_page_count: int):
+    # find <!-- geulgyeol-read-end -->0<!-- geulgyeol-read-end --> then update the page count, keeping the same format
+    # use regex to find and replace
+
+    pretty_number = f"{new_page_count:,}"
+    new_page = re.sub(r"(<!-- geulgyeol-read-end -->)([0-9,]+)(<!-- geulgyeol-read-end -->)",
+                        fr"\1{pretty_number}\3",
+                      old_page)
+    
+    return new_page
+
+def update():
+    files_data = fetch_files_data()
+    total = files_data["total"]
+
+    if not isinstance(total, int):
+        raise ValueError("total is not an integer.")
+    
+    readme_file = hf_api.hf_hub_download(repo_id=SPACE_NAME, filename="README.md", repo_type="space")
+    with open(readme_file, "r", encoding="utf-8") as f:
+        old_readme_content = f.read()
+    new_readme_content = get_updated_page_content(old_readme_content, total)
+    hf_api.upload_file(
+        path_or_fileobj=new_readme_content.encode("utf-8"),
+        path_in_repo="README.md",
+        repo_id=SPACE_NAME,
+        repo_type="space"
+    )
+
+def main():
+    update()
+
+if __name__ == "__main__":
+    main()
